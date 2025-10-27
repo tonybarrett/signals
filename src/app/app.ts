860f62fd16f5a483c2e9929e3c02c214fd4543d8
Tony Barrett <tonyb@gmx.com>
2025-10-27 10:03:12 +0000

    wip

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './components/main-component/main-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('signals');
 }
---
+++ src/app/components/main-component/main-component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FirstComponent } from '../first-component/first-component';
+import { SecondComponent } from '../second-component/second-component';
+
+const myComponents = [
+  FirstComponent,
+  SecondComponent
+];
+
+@Component({
+  selector: 'app-main-component',
+  standalone: true,
+  imports: [myComponents],
+  templateUrl: './main-component.html',
+  styleUrl: './main-component.scss',
+})
+export class MainComponent {
+
+}
